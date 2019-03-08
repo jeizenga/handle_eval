9088beac3af85d39f5e1b5a3740ee530ce87860e
emikoifish <emikoifish@gmail.com>
2019-03-07 18:31:28 -0800

    updating project for odgi

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
 
 FINDLIB=
 FINDLIB+=-L vg/lib
+FINDLIB+=-L /usr/lib
 FINDLIB+=-L /usr/local/lib
 FINDLIB+=-L /opt/local/lib
-FINDLIB+=-L /opt/local/lib/libomp -lomp
+FINDLIB+=-L /opt/local/lib/libomp
 
 LIB=
 LIB+=-lvg
-LIB+=-lvcflib -lgssw -lssw -lprotobuf -lsublinearLS -lhts -ldeflate -lpthread -ljansson -lncurses -lgcsa2 -lgbwt -ldivsufsort -ldivsufsort64 -lvcfh -lgfakluge -lraptor2 -lsdsl -lpinchesandcacti -l3edgeconnected -lsonlib -lfml -llz4 -lstructures -lvw -lboost_program_options -lallreduce
+LIB+=-lvcflib -lgssw -lssw -lprotobuf -lsublinearLS -lhts -ldeflate -lpthread -ljansson -lncurses -lgcsa2 -lgbwt -ldivsufsort -ldivsufsort64 -lvcfh -lgfakluge -lraptor2 -lsdsl -lpinchesandcacti -l3edgeconnected -lsonlib -lfml -llz4 -lstructures -lvw -lboost_program_options -lallreduce -lz -lbz2 -llzma -lhandlegraph -lomp -lstdc++
+LIB+=-lcairo -lz -lgobject-2.0 -lffi -lglib-2.0 -lintl -lpcre -lintl  -liconv -lpixman-1 -lfontconfig -liconv -lexpat -luuid -lfreetype -lbz2 -lpng16 -lz -lX11-xcb -lxcb-render -lXrender -lXext -lX11 -lxcb -lXau -lXdmcp -ldl -llzma -lrocksdb  -lsnappy -lz -lbz2 -llz4 
 
 INC=
 INC+=-I dankgraph/dynamic-prefix/src/dynamic/include
@@ -15,6 +17,10 @@ INC+=-I dankgraph/gfakluge-prefix/src/gfakluge/src
 INC+=-I dankgraph/sdsl-lite-prefix/src/sdsl-lite/src
 INC+=-I dankgraph/sparsepp-prefix/src/sparsepp/sparsepp
 INC+=-I dankgraph/tayweeargs-prefix/src/tayweeargs
+INC+=-I dankgraph/handlegraph-prefix/src
+INC+=-I dankgraph/backwardscpp-prefix/src
+INC+=-I dankgraph/bbhash-prefix/src
+INC+=-I dankgraph/bsort-prefix/src
 INC+=-I dankgraph/ska-prefix/src/ska
 INC+=-I dankgraph/src
 INC+=-I vg/include
@@ -24,6 +30,13 @@ INC+=-I vg/include/dynamic
 INC+=-I vg/include/sonLib
 INC+=-I vg/src
 INC+=-I vg/src/algorithms
+INC+=-I /usr/local/include
+INC+=-I /opt/local/include/pixman-1
+INC+=-I /opt/local/include/ossp
+INC+=-I /opt/local/include/freetype2
+INC+=-I /opt/local/include/libpng16
+INC+=-I /opt/local/include/cairo
+INC+=-I /opt/local/include/glib-2.0 -I/opt/local/lib/glib-2.0/include
 
 
 CXXFLAGS=
@@ -40,4 +53,4 @@ dankgraph/CMakeFiles/dg.dir/src/graph.cpp.o: dankgraph/src/*
 
 bin/project: src/project.cpp vg/lib/libvg.a dankgraph/CMakeFiles/dg.dir/src/graph.cpp.o
 	g++ src/project.cpp $(INC) $(CXXFLAGS) -c -o obj/project.o
-	g++ obj/project.o dankgraph/CMakeFiles/dg.dir/src/graph.cpp.o $(FINDLIB) $(LIB) $(CXXFLAGS) -o bin/project
+	g++ obj/project.o /opt/local/lib/libstdc++.6.dylib dankgraph/CMakeFiles/dg.dir/src/graph.cpp.o $(FINDLIB) $(LIB) $(CXXFLAGS) -o bin/project
--- src/project.cpp
+++ src/project.cpp
@@ -18,8 +18,9 @@ using namespace std;
 using namespace xg;
 using namespace vg;
 using namespace handlegraph;
+using namespace odgi;
 void help_me(char** argv) {
-    cerr << "usage: " << argv[0] << " project [in_option] input_file [out_options]" << endl;
+    cerr << "usage: " << argv[0] << " project [out_options] input_file" << endl;
 }
 
 
@@ -29,47 +30,30 @@ int main(int argc, char** argv) {
         help_me(argv);
         return 1;
     }
-    if (argc < 4) {
+    if (argc < 3) {
         help_me(argv);
         return 1;
     }
     
-    string vg_in;
-    string xg_in;
+    string vg_in = argv[2];
     bool vg_out = false;
-    bool xg_out = false;
     bool pg_out = false;
     bool hg_out = false;
     bool dg_out = false;
-    int c = atoi(argv[1]);
     
-    // look at in commands
-    switch (c){
-        case 1:
-            vg_in = argv[2];
-            break;
-        case 2:
-            xg_in = argv[2];
-            break;
-        default:
-            abort();
-    }
-    c = atoi(argv[3]);
+    int c = atoi(argv[1]);
     // look at out commands
     switch(c){
         case 1:
             vg_out = true;
             break;
         case 2:
-            xg_out = true;
-            break;
-        case 3:
             pg_out = true;
             break;
-        case 4:
+        case 3:
             hg_out = true;
             break;
-        case 5:
+        case 4:
             dg_out = true;
             break;
         default:
@@ -77,42 +61,41 @@ int main(int argc, char** argv) {
     }
     
     cerr << "vg_in" << vg_in << endl;
-    cerr << "xg_in" << xg_in << endl;
     cerr << "vg_out" << vg_out << endl;
-    cerr << "xg_out" << xg_out << endl;
     cerr << "pg_out" << pg_out << endl;
     cerr << "hg_out" << hg_out << endl;
     cerr << "dg_out" << dg_out << endl;
 
     
-    vg::VG* graph;
-    if (!vg_in.empty()){
-        ifstream in(vg_in);
-        graph->from_istream(in);
-    }
     
-    
+//    if (!vg_in.empty()){
+//        ifstream in(vg_in);
+//        VG* vg = new VG(in);
+//    }
+    ifstream in(vg_in);
+//    VG* vg = new VG(in);
+//    delete vg;
+    VG graph(in);
+   
 //    VG* vg;
 //    convert_handle_graph(&graph, &vg);
 //    // let's try converting
-//    if (vg_out){
-//        VG vg;
-//        convert_handle_graph(&test_vg, &vg);
-//    }
-//    else if (xg_out){
-//        XG xg;
-//        convert_handle_graph(&graph, &vg);
-//    }
-//    else if (pg_out){
-//        PackedGraph pg;
-//        convert_handle_graph(&graph, &pg);
-//    }
-//    else if (hg_out){
-//        HashGraph hg;
-//        convert_handle_graph(&graph, &hg);
-//    }
-//    else if (dg_out){
-//    }
+    if (vg_out){
+        VG vg;
+        convert_handle_graph(&graph, &vg);
+    }
+    else if (pg_out){
+        PackedGraph pg;
+        convert_handle_graph(&graph, &pg);
+    }
+    else if (hg_out){
+        HashGraph hg;
+        convert_handle_graph(&graph, &hg);
+    }
+    else if (dg_out){
+        graph_t dg;
+        convert_handle_graph(&graph, &dg);
+    }
 //    else {
 //
 //    }
