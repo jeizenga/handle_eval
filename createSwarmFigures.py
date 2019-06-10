3fabab76e2ba6e2294f248264d6dc8dda7017705
emikoifish <emikoifish@gmail.com>
2019-06-10 11:42:34 -0700

    updated figures and added a swarmplot version

--- createSwarmFigures.py
+++ createSwarmFigures.py
@@ -10,8 +10,6 @@ import argparse
 import random
 import math
 import pandas
-import datetime
-import time
 
 
 def createFigure(inputFile, outputDirectory):
@@ -81,20 +79,18 @@ def createFigure(inputFile, outputDirectory):
                     graphName = ".og"
 
                 deserializeFileGraphData = deserializeData.loc[df["file name"] == fileName[:-3]+graphName].loc[df["graph type"] == graphType]
-                utilize[graphType] = np.mean(deserializeFileGraphData["max memory"])
+                # utilize[graphType] = np.mean(deserializeFileGraphData["max memory"])
+                utilize[graphType] = [deserializeFileGraphData["max memory"]]
                 convertFileGraphData  = convertFileData.loc[df["graph type"] == graphType]
-                memory[graphType] = np.mean(convertFileGraphData["max memory"])
+                # memory[graphType] = np.mean(convertFileGraphData["max memory"])
+                memory[graphType] = [convertFileGraphData["max memory"]]
                 # speed[graphType] = np.mean(convertFileGraphData["sys"] + convertFileGraphData["usr"])
                 speed[graphType] = [convertFileGraphData["sys"] + convertFileGraphData["usr"]]
 
                 for testType in [3,4,5]:
                     dataFileGraphData = data[testType].loc[df["file name"] == fileName[:-3] + graphName].loc[df["graph type"] == graphType]
                     if testType == 3:
-
-                        x = time.strptime(dataFileGraphData["nodes/edges/paths time"].split(',')[0],'%H:%M:%S')
-
-                        nodes[graphType] = [datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()] /np.mean(dataFileGraphData["nodes/edges/paths"].astype(np.float))
-                        print(nodes[graphType])
+                        nodes[graphType] = [dataFileGraphData["nodes/edges/paths time"].astype(np.float)] /np.mean(dataFileGraphData["nodes/edges/paths"].astype(np.float))
                     elif testType ==4:
                         edges[graphType] = [dataFileGraphData["nodes/edges/paths time"].astype(np.float)] /np.mean(dataFileGraphData["nodes/edges/paths"].astype(np.float))
                     elif testType == 5:
@@ -127,51 +123,54 @@ def createFigure(inputFile, outputDirectory):
             bins = np.arange(0, len(memory)+1, 1)
             errorBarWidth = .015
             errorBins = [ b+.5 - errorBarWidth/2 for b in bins]
-            errorBars = []
+            memoryError = []
+            maxMemory = 0
+            speedError = []
+            maxSpeed = 0
+            utilizeError = []
+            maxUtilize = 0
+
+            for index in range(0,len(memory)):
+                memoryHeight = np.mean(memory[index + 1])
+                memoryStd = np.std(memory[index + 1])
+                memoryError.append(memoryHeight+memoryStd)
+                maxMemory = max(maxMemory, max(memory[index + 1][0]))
+
+                speedHeight = np.mean(speed[index + 1])
+                speedStd = np.std(speed[index + 1])
+                speedError.append(speedHeight + speedStd)
+                maxSpeed = max(maxSpeed, max(speed[index + 1][0]))
+
+                utilizeHeight = np.mean(utilize[index + 1])
+                utilizeStd = np.std(utilize[index + 1])
+                utilizeError.append(utilizeHeight + utilizeStd)
+                maxUtilize = max(maxUtilize, max(utilize[index + 1][0]))
+
 
             for index in range(0, len(memory)):
                 bottom = 0
                 left = bins[index] + .1
                 width = bins[index + 1] - left - .1
                 errorBarLeft = errorBins[index]
+                # print(0, max(bins), 0, max(memoryError))
+                swarmplot(panel1, memory[index+1], bins[index] + .5, 2, 2, 0, max(bins),
+                          0, maxMemory * 1.05, 1, 3, (R[index], G[index], B[index]))
 
-                height = memory[index+1]
-                rectangle1 = mplpatches.Rectangle([left, bottom], width, height,
-                                                  edgecolor="black",
-                                                  facecolor=(R[index], G[index], B[index]),
-                                                  linewidth=.1)
-                panel1.add_patch(rectangle1)
-                speedHeight = np.mean(speed[index+1])
-                rectangle2 = mplpatches.Rectangle([left, bottom], width, speedHeight,
-                                                  edgecolor="black",
-                                                  facecolor=(R[index], G[index], B[index]),
-                                                  linewidth=.1)
-                panel2.add_patch(rectangle2)
-                speedStd = np.std(speed[index + 1])
-                errorBar = mplpatches.Rectangle([errorBarLeft, speedHeight-speedStd], errorBarWidth, speedStd*2,
-                                                  edgecolor="black",
-                                                  facecolor="black",
-                                                  linewidth=.1)
-                errorBars.append(speedHeight+speedStd)
-                panel2.add_patch(errorBar)
+                swarmplot(panel2, speed[index + 1], bins[index]+.5, 2, 2, 0, max(bins),
+                          0, maxSpeed * 1.05, .5, 3, (R[index], G[index], B[index]))
 
-                utilizeHeight = utilize[index + 1]
-                rectangle3 = mplpatches.Rectangle([left, bottom], width,
-                                                  utilizeHeight,
-                                                  edgecolor="black",
-                                                  facecolor=(R[index], G[index], B[index]),
-                                                  linewidth=.1)
-                panel3.add_patch(rectangle3)
+                swarmplot(panel3, utilize[index + 1], bins[index] + .5, 2, 2, 0, max(bins),
+                          0, maxUtilize * 1.05, .5, 3, (R[index], G[index], B[index]))
 
-            scientific = np.format_float_scientific(max(memory.values())*1.1)
+            scientific = np.format_float_scientific(maxMemory*1.05)
             power = int(scientific.split("e+")[-1])
-            panel1.set_ylim(0, max(memory.values()) * 1.1)
+            panel1.set_ylim(0, maxMemory * 1.05)
             panel1.set_xlim(0, max(bins))
 
-            roundedNumber = special_round(max(memory.values()) * 1.1, 10**power)
+            roundedNumber = special_round(maxMemory * 1.05, 10**power)
             panel1.set_yticks([a for a in np.linspace(0, roundedNumber, 5)])
             panel1.set_yticklabels([a for a in np.linspace(0, roundedNumber/10**power,  5)])
-            panel1.set_ylabel("Maximum Memory ($10^{" + str(power) + "}$ bytes)", )
+            panel1.set_ylabel("Maximum Memory ($10^{"+str(power)+"}$ bytes)",)
             panel1.set_title("Construction Memory Usage")
             # offset = panel1.get_yaxis().get_offset_text()
             # print(offset, offset.get_text())
@@ -184,7 +183,7 @@ def createFigure(inputFile, outputDirectory):
                                right=False, labelright=False,
                                top=False, labeltop=False
                                )
-            panel2.set_ylim(0, max(errorBars) * 1.05)
+            panel2.set_ylim(0, maxSpeed * 1.05)
             panel2.set_xlim(0, max(bins))
             panel2.set_ylabel("Time (CPU cycles)")
             # modify tick marks/labeling
@@ -195,12 +194,12 @@ def createFigure(inputFile, outputDirectory):
                                top=False, labeltop=False
                                )
 
-            scientific = np.format_float_scientific(max(utilize.values()) * 1.1)
+            scientific = np.format_float_scientific(max(utilizeError) * 1.1)
             power = int(scientific.split("e+")[-1])
-            panel3.set_ylim(0, max(utilize.values()) * 1.1)
+            panel3.set_ylim(0, maxUtilize * 1.05)
             panel3.set_xlim(0, max(bins))
 
-            roundedNumber = special_round(max(utilize.values()) * 1.1, 10 ** power)
+            roundedNumber = special_round(maxUtilize * 1.05, 10 ** power)
             panel3.set_yticks([a for a in np.linspace(0, roundedNumber, 5)])
             panel3.set_yticklabels([a for a in np.linspace(0, roundedNumber / 10 ** power, 5)])
             panel3.set_ylabel("Maximum Memory ($10^{" + str(power) + "}$ bytes)", )
@@ -221,7 +220,19 @@ def createFigure(inputFile, outputDirectory):
             errorBins = [b + .5 - errorBarWidth / 2 for b in bins]
             errorBars = []
             counter = 0
+            maxnep = 0
 
+            for nep in [nodes, edges, paths]:
+                nepIndexing = 0
+                for index in range(len(nodes) *(counter), len(nodes) * (counter+1)):
+                    height = np.mean(nep[nepIndexing + 1])
+                    speedStd = np.std(nep[nepIndexing + 1])
+                    errorBars.append(height + speedStd)
+                    maxnep = max(maxnep, max(nep[nepIndexing + 1][0]))
+                    nepIndexing += 1
+                counter += 1
+
+            counter = 0
             for nep in [nodes, edges, paths]:
                 nepIndexing = 0
                 for index in range(len(nodes) *(counter), len(nodes) * (counter+1)):
@@ -230,26 +241,29 @@ def createFigure(inputFile, outputDirectory):
                     width = bins[index + 1] - left
                     height = np.mean(nep[nepIndexing +1])
                     errorBarLeft = errorBins[index]
+                    #
+                    # rectangle1 = mplpatches.Rectangle([left, bottom], width,
+                    #                                   height,
+                    #                                   edgecolor="black",
+                    #                                   facecolor=(R[nepIndexing], G[nepIndexing], B[nepIndexing]),
+                    #                                   linewidth=.1)
+                    # panel4.add_patch(rectangle1)
+                    #
+                    # speedStd = np.std(nep[nepIndexing +1])
+                    # errorBar = mplpatches.Rectangle([errorBarLeft, height - speedStd], errorBarWidth,
+                    #                                 speedStd * 2,
+                    #                                 edgecolor="black",
+                    #                                 facecolor="black",
+                    #                                 linewidth=.1)
+                    # errorBars.append(height + speedStd)
+                    # panel4.add_patch(errorBar)
 
-                    rectangle1 = mplpatches.Rectangle([left, bottom], width,
-                                                      height,
-                                                      edgecolor="black",
-                                                      facecolor=(R[nepIndexing], G[nepIndexing], B[nepIndexing]),
-                                                      linewidth=.1)
-                    panel4.add_patch(rectangle1)
-
-                    speedStd = np.std(nep[nepIndexing +1])
-                    errorBar = mplpatches.Rectangle([errorBarLeft, height - speedStd], errorBarWidth,
-                                                    speedStd * 2,
-                                                    edgecolor="black",
-                                                    facecolor="black",
-                                                    linewidth=.1)
-                    errorBars.append(height + speedStd)
-                    panel4.add_patch(errorBar)
+                    swarmplot(panel4, nep[nepIndexing +1], bins[index] + .5, 2, 2, 0, max(bins),
+                              0, maxnep * 1.05, .5, 3, (R[nepIndexing], G[nepIndexing], B[nepIndexing]))
 
                     nepIndexing += 1
                 counter += 1
-            panel4.set_ylim(0, max(errorBars) * 1.1)
+            panel4.set_ylim(0, maxnep * 1.05)
             panel4.set_xlim(0, max(bins))
             panel4.set_ylabel("Time per Access (seconds)")
             panel4.set_title("Access Time")
@@ -266,6 +280,78 @@ def createFigure(inputFile, outputDirectory):
             # save plot to output file
             plt.savefig(outputDirectory+"/"+fileName[:-3]+".png", dpi=600)
 
+
+def swarmplot(panel, data, xcoord, pw, ph, xmin, xmax, ymin, ymax, space, size, color):
+    """
+    Swarmplot function from Vollmers.
+    """
+    data = np.array(data)[0]
+    data = sorted(data)
+    xr = xmax - xmin
+    yr = ymax - ymin
+    min_dist = size / 75
+    increment = size / 200
+
+    potential_x_positions = []
+    for x_shift in np.arange(0, space, increment):
+        potential_x_positions.append(xcoord + x_shift)
+        potential_x_positions.append(xcoord - x_shift)
+
+    plotted_points = []
+
+    for y1 in data:
+        if len(plotted_points) == 0:
+            plotted_points.append((xcoord, y1))
+        else:
+            compare_points = []
+            for x2, y2 in plotted_points:
+                ydist = ((((y1 - y2) / yr) * ph) ** 2) ** 0.5
+                if ydist < min_dist:
+                    compare_points.append((x2, y2))
+            if len(compare_points) > 0:
+                for x1 in potential_x_positions:
+                    distances = []
+                    remove_list = []
+                    for x2, y2 in compare_points:
+                        xdist = (((x1 - x2) / xr) * pw)
+                        ydist = (((y1 - y2) / yr) * ph)
+                        distance = ((xdist ** 2) + (ydist ** 2)) ** 0.5
+                        distances.append(distance)
+                        if distance > min_dist:
+                            if x1 > x2 and x2 > xcoord:
+                                remove_list.append((x2, y2))
+                            if x1 < x2 and x2 < xcoord:
+                                remove_list.append((x2, y2))
+                        else:
+                            break
+
+                    if min(distances) > min_dist:
+                        plotted_points.append((x1, y1))
+                        break
+                    else:
+                        for point in remove_list:
+                            compare_points.remove(point)
+            else:
+                plotted_points.append((xcoord, y1))
+
+    x_list = []
+    y_list = []
+    for x, y in plotted_points:
+        x_list.append(x)
+        y_list.append(y)
+
+    panel.plot(x_list, y_list, markersize=size, marker='o', markeredgewidth=0,
+               color=color, linewidth=0)
+
+
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return (s, size_name[i])
 
 def argParser():
     parser = argparse.ArgumentParser(add_help=True)
