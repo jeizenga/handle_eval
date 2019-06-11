97bc38774e19a090c925008c7c90fed1ac4e2987
Jordan Eizenga <joeizeng@gmail.com>
2019-06-11 12:48:03 -0700

    remove case/switch for strings

--- src/eval.cpp
+++ src/eval.cpp
@@ -34,24 +34,23 @@ void convert_graphs_test(string& output_format, string& input_file, bool make_se
     bool hg_out = false;
     bool og_out = false;
     
-    switch(output_format){
-        case "none":
-            quit_out = true;
-            break;
-        case "vg":
-            vg_out = true;
-            break;
-        case "pg":
-            pg_out = true;
-            break;
-        case "hg":
-            hg_out = true;
-            break;
-        case "og":
-            og_out = true;
-            break;
-        default:
-            abort();
+    if (output_format == "none") {
+        quit_out = true;
+    }
+    else if (output_format == "vg") {
+        vg_out = true;
+    }
+    else if (output_format == "pg") {
+        pg_out = true;
+    }
+    else if (output_format == "hg") {
+        hg_out = true;
+    }
+    else if (output_format == "og") {
+        og_out = true;
+    }
+    else {
+        abort();
     }
     
     ifstream in(input_file);
@@ -92,78 +91,32 @@ void convert_graphs_test(string& output_format, string& input_file, bool make_se
 //    }
 }
 
-void make_deserialize_test(string& convert_type, string& input_file){
+void test_from_serialized(string& serlialized_type, string& input_file, string& test_name) {
     
     bool quit_in = false;
     bool vg_in = false;
     bool pg_in = false;
     bool hg_in = false;
     bool og_in = false;
-    switch(convert_type){
-        case "none":
-            quit_in = true;
-            break;
-        case "vg":
-            vg_in = true;
-            break;
-        case "pg":
-            pg_in = true;
-            break;
-        case "hg":
-            hg_in = true;
-            break;
-        case "og":
-            og_in = true;
-            break;
-        default:
-            abort();
-    }
-    ifstream in(input_file);
-    if (vg_in){
-        VG vg;
-        vg.deserialize(in);
-    }
-    else if (pg_in){
-        PackedGraph pg;
-        pg.deserialize(in);
-    }
-    else if (hg_in){
-        HashGraph hg;
-        hg.deserialize(in);
-    }
-//    else if (og_in){
-//        graph_t dg;
-//        dg.load(in);
-//    }
     
-}
-
-void make_nodes_per_second(string& convert_type, string& input_file){
-    bool quit_in = false;
-    bool vg_in = false;
-    bool pg_in = false;
-    bool hg_in = false;
-    bool og_in = false;
-    switch(convert_type){
-        case "none":
-            quit_in = true;
-            break;
-        case "vg":
-            vg_in = true;
-            break;
-        case "pg":
-            pg_in = true;
-            break;
-        case "hg":
-            hg_in = true;
-            break;
-        case "og":
-            og_in = true;
-            break;
-        default:
-            abort();
+    if (convert_type == "none") {
+        quit_in = true;
     }
-    ifstream in(input_file);
+    else if (convert_type == "vg") {
+        vg_in = true;
+    }
+    else if (convert_type == "pg") {
+        pg_in = true;
+    }
+    else if (convert_type == "hg") {
+        hg_in = true;
+    }
+    else if (convert_type == "og") {
+        og_in = true;
+    }
+    else {
+        abort();
+    }
     
     VG* vg_graph = nullptr;
     PackedGraph* pg  = nullptr;
@@ -173,6 +126,7 @@ void make_nodes_per_second(string& convert_type, string& input_file){
     
     PathHandleGraph* test_graph = nullptr;
     
+    ifstream in(input_file);
     if (vg_in) {
         vg_graph = new VG();
         vg->deserialize(in);
@@ -193,160 +147,61 @@ void make_nodes_per_second(string& convert_type, string& input_file){
 //        test_graph = dg;
 //    }
     
-    int node_counter = 0;
-    auto start = std::chrono::system_clock::now();
-    test_graph->for_each_handle([&](const handle_t& h) {
-        node_counter++;
-    });
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    cerr << "number of nodes accessed: " << node_counter << endl;
-    cerr << "elapsed time: " << elapsed_seconds.count() << endl;
-}
-
-void make_edges_per_second(string& convert_type, string& input_file){
-    bool quit_in = false;
-    bool vg_in = false;
-    bool pg_in = false;
-    bool hg_in = false;
-    bool og_in = false;
-    switch(convert_type){
-        case "none":
-            quit_in = true;
-            break;
-        case "vg":
-            vg_in = true;
-            break;
-        case "pg":
-            pg_in = true;
-            break;
-        case "hg":
-            hg_in = true;
-            break;
-        case "og":
-            og_in = true;
-            break;
-        default:
-            abort();
+    if (test_name == "deserialize") {
+        // we're already done
     }
-    ifstream in(input_file);
-
-    VG* vg_graph = nullptr;
-    PackedGraph* pg  = nullptr;
-    HashGraph* hg = nullptr;
-//    graph_t dg = nullptr;
-
-
-    PathHandleGraph* test_graph = nullptr;
-
-    if (vg_in) {
-        vg_graph = new VG(in);
-        test_graph = vg_graph;
-    }
-    else if (pg_in){
-        pg = new PackedGraph();
-        pg->deserialize(in);
-        test_graph = pg;
-    }
-    else if (hg_in){
-        hg = new HashGraph();
-        hg->deserialize(in);
-        test_graph = hg;
-    }
-//    else if (og_in){
-//        dg = new graph_t(in);
-//        test_graph = dg;
-//    }
-
-    vector<handle_t> handles = {};
-    test_graph->for_each_handle([&](const handle_t& h) {
-        for (bool orientation : {false, true}) {
-            handles.push_back(test_graph->get_handle(test_graph->get_id(h), orientation));
-        }
-    });
-
-    int edge_counter = 0;
-    auto start = std::chrono::system_clock::now();
-    for(handle_t handle:handles){
-        test_graph->follow_edges(handle, true, [&](const handle_t& next) {
-            edge_counter++;
+    else if (test_name == "nodes") {
+        
+        int node_counter = 0;
+        auto start = std::chrono::system_clock::now();
+        test_graph->for_each_handle([&](const handle_t& h) {
+            node_counter++;
         });
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        cerr << "number of nodes accessed: " << node_counter << endl;
+        cerr << "elapsed time: " << elapsed_seconds.count() << endl;
+        
     }
-    auto end = std::chrono::system_clock::now();
-    
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    cerr << "number of edges accessed: " << edge_counter << endl;
-    cerr << "elapsed time: " << elapsed_seconds.count() << endl;
-}
-
-void make_paths_per_second(string& convert_type, string& input_file){
-    bool quit_in = false;
-    bool vg_in = false;
-    bool pg_in = false;
-    bool hg_in = false;
-    bool og_in = false;
-    switch(convert_type){
-        case "none":
-            quit_in = true;
-            break;
-        case "vg":
-            vg_in = true;
-            break;
-        case "pg":
-            pg_in = true;
-            break;
-        case "hg":
-            hg_in = true;
-            break;
-        case "og":
-            og_in = true;
-            break;
-        default:
-            abort();
-    }
-    ifstream in(input_file);
-
-    VG* vg_graph = nullptr;
-    PackedGraph* pg  = nullptr;
-    HashGraph* hg = nullptr;
-    //    graph_t dg = nullptr;
-
-
-    PathHandleGraph* test_graph = nullptr;
-
-    if (vg_in) {
-        vg_graph = new VG(in);
-        test_graph = vg_graph;
-    }
-    else if (pg_in){
-        pg = new PackedGraph();
-        pg->deserialize(in);
-        test_graph = pg;
-    }
-    else if (hg_in){
-        hg = new HashGraph();
-        hg->deserialize(in);
-        test_graph = hg;
-    }
-//    else if (og_in){
-//        dg = new graph_t(in);
-//        test_graph = dg;
-//    }
-
-    int path_counter = 0;
-    auto start = std::chrono::system_clock::now();
-    test_graph->for_each_path_handle([&](const path_handle_t& path_handle_1){
-        for (handle_t handle : test_graph->scan_path(path_handle_1)) {
-            volatile handle_t temp_handle = handle;
+    else if (test_name == "edges") {
+        
+        vector<handle_t> handles;
+        test_graph->for_each_handle([&](const handle_t& h) {
+            for (bool orientation : {false, true}) {
+                handles.push_back(test_graph->get_handle(test_graph->get_id(h), orientation));
+            }
+        });
+        
+        int edge_counter = 0;
+        auto start = std::chrono::system_clock::now();
+        for(handle_t handle:handles){
+            test_graph->follow_edges(handle, true, [&](const handle_t& next) {
+                edge_counter++;
+            });
         }
-        path_counter++;
-    });
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    cerr << "number of paths accessed: " << path_counter << endl;
-    cerr << "elapsed time: " << elapsed_seconds.count() << endl;
+        auto end = std::chrono::system_clock::now();
+        
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        cerr << "number of edges accessed: " << edge_counter << endl;
+        cerr << "elapsed time: " << elapsed_seconds.count() << endl;
+    }
+    else if (test_name == "paths") {
+        
+        int path_counter = 0;
+        auto start = std::chrono::system_clock::now();
+        test_graph->for_each_path_handle([&](const path_handle_t& path_handle_1){
+            for (handle_t handle : test_graph->scan_path(path_handle_1)) {
+                volatile handle_t temp_handle = handle;
+            }
+            path_counter++;
+        });
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        cerr << "number of paths accessed: " << path_counter << endl;
+        cerr << "elapsed time: " << elapsed_seconds.count() << endl;
+    }
 }
-
+\
 
 int main(int argc, char** argv) {
     
@@ -367,52 +222,20 @@ int main(int argc, char** argv) {
     bool nodes_per_second = false;
     bool edges_per_second = false;
     bool paths_per_second = false;
-    // look at out commands
-    switch(argv[1]){
-        case "convert":
-            convert_graphs = true;
-            break;
-        case "serialize":
-            make_serialized = true;
-            break;
-        case "deserialize":
-            make_deserialize = true;
-            break;
-        case "nodes":
-            nodes_per_second = true;
-            break;
-        case "edges":
-            edges_per_second = true;
-            break;
-        case "paths":
-            paths_per_second = true;
-            break;
-        default:
-            abort();
-    }
-    
-    
+        
     // for converting to another graph
+    string test_name = argv[1];
     string convert_type = argv[2];
     string input_file = argv[3];
     
-    if (convert_graphs){
+    // look at out commands
+    if (test_name == "convert"){
         convert_graphs_test(convert_type, input_file, false);
     }
-    else if (make_serialized){
+    else if (test_name == "serialize"){
         convert_graphs_test(convert_type, input_file, true);
-    
     }
-    else if (make_deserialize){
-        make_deserialize_test(convert_type, input_file);
-    }
-    else if (nodes_per_second){
-        make_nodes_per_second(convert_type, input_file);
-    }
-    else if (edges_per_second){
-        make_edges_per_second(convert_type, input_file);
-    }
-    else if (paths_per_second){
-        make_paths_per_second(convert_type, input_file);
+    else {
+        test_from_serialized(convert_type, input_file, test_name);
     }
 }
